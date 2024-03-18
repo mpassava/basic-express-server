422f1d30647de5cd630dc7254646009c131ad82d
mpassava <mpassava@gmail.com>
2024-03-18 13:35:11 -0400

    created users page

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
-const method = require('./middleware/method.js');
+const method = require("./middleware/method.js");
+const userData = require("./users.json");
 
-app.get('/', method, (req, res) => {
-  res.send('Hello API');
-})
+app.get("/", method, (req, res) => {
+  res.send("Hello API");
+});
+
+app.get("/users", method, (req, res) => {
+  res.json(userData);
+});
 
 app.listen(3000, (err) => {
   if (err) {
